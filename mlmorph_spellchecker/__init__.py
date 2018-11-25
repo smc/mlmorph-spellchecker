2a9c0629621c674622335be51b85f560a5778c12
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-11-25 15:46:52 +0530

    Sort the suggestions based on analysis weight

--- mlmorph_spellchecker/__init__.py
+++ mlmorph_spellchecker/__init__.py
@@ -9,6 +9,7 @@ import abc
 import importlib
 from .strategies import *
 
+
 class Suggestion:
     """
     Define the interface of interest to clients.
@@ -22,30 +23,35 @@ class Suggestion:
         return self._strategy.suggest(word)
 
 
-def getSuggestions(word, morph):
+def getSuggestions(word, analyser):
     # Order of the items in STRATEGIES is important
     STRATEGIES = ['VisualSimilarity', 'Ykkuka', 'TTaCorrection', 'NtaCorrection', 'MpaCorrection', 'LLaCorrection',
                   'GeminateConsonants', 'ViramaInsertion', 'VowelElongation', 'VowelShortening',
                   'ChilluToConsonantVirama', 'ConsonantViramaToChillu',
                   'ChilluNormalization',
                   'ConsonantAspirationCorrection', 'ConsonantVoiceCorrection']
-    suggestions = []
+
+    weighted_suggestions = {}
     for i in range(len(STRATEGIES)):
         strategy = globals()[STRATEGIES[i]]()
         context = Suggestion(strategy)
         candidates = context.suggest(word)
         for candidate in candidates:
-            if spellcheck(candidate, morph) is True:
-                if not candidate in suggestions:
-                    suggestions.append(candidate)
-    return suggestions
+            if candidate in weighted_suggestions:
+                continue
+            weighted_analysis = analyser.analyse(candidate, True)
+            if len(weighted_analysis) > 0:
+                weighted_suggestions[candidate] = weighted_analysis[0][1]
+
+    # Sort by the increasing order of weights
+    suggestions = sorted(weighted_suggestions.items(), key=lambda t: t[1])
+    # Return the words array
+    return [suggestion[0] for suggestion in suggestions]
 
 
 def spellcheck(word, analyser):
     analysis = analyser.analyse(word, False)
-    if len(analysis) == 0:
-        return False
-    else:
-        return True
+    return len(analysis) > 0
+
 
 __all__ = ['spellcheck', 'getSuggestions']
