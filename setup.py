238484eb20dd3151ddec9f579f5210c962c2b15a
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-12-25 10:11:26 +0530

    Update version

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     zip_safe=False,
+    version=open('VERSION').read().strip(),
     entry_points={
         'console_scripts': [
             'spellchecker = mlmorph_spellchecker.__main__:main'
